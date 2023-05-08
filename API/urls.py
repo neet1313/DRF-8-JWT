018fa2bf18a8dd4fbc42ab7114f23bf44ed30251
Prabhneet <prabhneet13@gmail.com>
2023-05-08 15:19:04 +0530

    Configure JWT

--- API/urls.py
+++ API/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from API import views
-# from rest_framework.authtoken.views import obtain_auth_token
-# from API.customauthtoken import CustomAuthToken
+
+# Importing predefined classes
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = DefaultRouter()
 router.register('api', views.StudentViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
-
-    # Add path to for Token Authentication
-    # path('gettoken/', obtain_auth_token)
-    
-    # Path for Custom Token respose
-    # path('gettoken/', CustomAuthToken.as_view())
+    path('gettoken', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refreshtoken', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verifytoken', TokenVerifyView.as_view(), name='token_verify')
 ]
+
+
+# Install JWT -> pip install djangorestframework-simplejwt
